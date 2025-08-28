c2072585cf006ba71e1eee4eb1f227081225a379
unknown <ab261099tro@H-ab261099tro.ws.a-bank.com.ua>
2025-08-28 22:39:17 +0300

    finish

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -15,7 +15,7 @@ export const authenticate = async (req, res, next) => {
     const bearer = authHeader.split(" ")[0];
     const token = authHeader.split(" ")[1];
 
-    if (bearer !== "Bearere" || !token) {
+    if (bearer !== "Bearer" || !token) {
         next(createHttpError(401, "Auth header should be of type Bearer"));
         return;
     }
